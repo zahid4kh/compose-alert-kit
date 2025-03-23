604c622980ba5a803527858cf30644953f61426b
zahid4kh <halilzahid@gmail.com>
2025-03-23 01:54:27 +0100

    add dependencies

--- composealertkit/build.gradle.kts
+++ composealertkit/build.gradle.kts
@@ -37,10 +37,15 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
