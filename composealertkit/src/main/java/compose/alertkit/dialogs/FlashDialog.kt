a79df295e9c1ec83b7608907fa549722f4e48bad
zahid4kh <halilzahid@gmail.com>
2025-03-23 04:27:50 +0100

    feat: add ErrorDialog composable
    - Added `ErrorDialog`: A composable function to display error messages.
    - Includes parameters for visibility, title, message, dismiss button text, and a dismiss callback.
    - Displays an error icon, title, message, and a dismiss button within a styled dialog.
    - Uses MaterialTheme for consistent theming.

---
+++ composealertkit/src/main/java/compose/alertkit/dialogs/FlashDialog.kt
@@ -0,0 +1,94 @@
+package compose.alertkit.dialogs
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import kotlinx.coroutines.delay
+
+
+/**
+ * A dialog that automatically dismisses after a specified duration.
+ * @param visible Whether the dialog is currently visible.
+ * @param message The message to display in the dialog.
+ * @param icon An optional icon to display in the dialog.
+ * @param duration The duration in milliseconds to automatically dismiss the dialog.
+ * @param onDismiss A callback that is invoked when the user dismisses the dialog.
+ */
+@Composable
+fun FlashDialog(
+    visible: Boolean,
+    message: String,
+    icon: @Composable () -> Unit = {},
+    duration: Long = 1500,
+    onDismiss: () -> Unit
+) {
+    var showDialog by remember { mutableStateOf(visible) }
+
+    LaunchedEffect(visible) {
+        showDialog = visible
+        if (visible) {
+            delay(duration)
+            showDialog = false
+            onDismiss()
+        }
+    }
+
+    AnimatedVisibility(
+        visible = showDialog,
+        enter = fadeIn() + scaleIn(initialScale = 0.8f),
+        exit = fadeOut() + scaleOut()
+    ) {
+        Dialog(
+            onDismissRequest = onDismiss,
+            properties = DialogProperties(dismissOnBackPress = false, dismissOnClickOutside = false)
+        ) {
+            Surface(
+                shape = RoundedCornerShape(16.dp),
+                color = MaterialTheme.colorScheme.surface
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(modifier = Modifier.size(40.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        icon()
+                    }
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Text(
+                        text = message,
+                        color = MaterialTheme.colorScheme.onSurface,
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            }
+        }
+    }
+}
